fc7aa0d54f6d6a6515a6bda300af0d5d1ef72939
hongwon1031 <hongwon1031@naver.com>
2025-05-06 01:57:58 +0900

    배포완료

---
+++ 20250505/app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from pydantic_settings import BaseSettings
+from models import Item
+
+class Settings(BaseSettings):
+    project_name : str
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+app = FastAPI(title = settings.project_name)
+
+fake_db = {}
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI app is running!"}
+
+@app.post("/items/", response_model = Item)
+def create_item(item:Item):
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail = 'Item already exists')
+    fake_db[item.id] = item
+    return item
+
+@app.get("/items/{item_id}", response_model = Item)
+def read_item(item_id : int):
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    return fake_db[item_id]
+
+@app.put("/items/{item_id}", response_model = Item)
+def update_item(item_id : int, item : Item):
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    fake_db[item_id] = item
+    return item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    del fake_db[item_id]
+    return {"message": "Item deleted"}
